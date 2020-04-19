1cb5eeefe49c28466339ecbc53f9a02010b45978
ordanax <ordanax@ya.ru>
2020-04-20 03:22:14 +0500

    Update a4uiso.sh

--- a4uiso.sh
+++ a4uiso.sh
@@ -2,6 +2,7 @@ mkdir ~/downloads
 cd ~/downloads
 pacman -S archiso
 cp -r /usr/share/archiso/configs/releng/ archlive
-
+wget https://github.com/a4uos/a4u_archiso/raw/master/archlive.tar.gz
+sudo tar -xzf archlive.tar.gz -C ~/
 mkdir ~/archlive/out/
 ./build.sh -v
