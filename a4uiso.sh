a29767e1c596de58e5605e3638c751966619ab45
ordanax <ordanax@ya.ru>
2020-04-26 21:51:37 +0500

    Delete a4uiso.sh

--- a4uiso.sh
+++
@@ -1,8 +0,0 @@
-mkdir ~/downloads
-cd ~/downloads
-sudo pacman -S archiso
-sudo cp -r /usr/share/archiso/configs/releng/ archlive
-wget https://github.com/a4uos/a4u_archiso/raw/master/archlive.tar.gz
-tar -xzf archlive.tar.gz -C ~/
-sudo mkdir ~/archlive/out/
-sudo ./build.sh -v
